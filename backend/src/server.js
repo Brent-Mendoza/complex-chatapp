aa4bf179a0ac66574e2d01de7fb30c60a4a879c9
Brent-Mendoza <mjustinebrent@gmail.com>
2025-09-17 18:15:30 +0800

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.route.js"
+import messageRouter from "./routes/message.route.js"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("api/v1/auth", authRouter)
+app.use("api/v1/messages", messageRouter)
+
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT)
+})

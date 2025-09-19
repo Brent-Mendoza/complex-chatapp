7faae111e97df7e3fc6ba2aa0881b8dfbd7f8835
Brent-Mendoza <mjustinebrent@gmail.com>
2025-09-19 18:46:32 +0800

    message routes

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,102 @@
+import cloudinary from "../lib/cloudinary.js"
+import Message from "../models/Message.js"
+import User from "../models/User.js"
+
+export const getAllContacts = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password")
+    res.status(200).json(filteredUsers)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: "Something went wrong" })
+  }
+}
+
+export const getMessageByUserId = async (req, res) => {
+  try {
+    const myId = req.user._id
+    const { id } = req.params
+    const message = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: id },
+        { senderId: id, receiverId: myId },
+      ],
+    })
+
+    res.status(200).json(message)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: "Something went wrong" })
+  }
+}
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body
+    const { id } = req.params
+    const senderId = req.user._id
+
+    if (!text && !image)
+      return res.status(400).json({ message: "Please fill all the fields" })
+
+    if (senderId.equals(id))
+      return res
+        .status(400)
+        .json({ message: "You can't send a message to yourself" })
+    const receiverExists = await User.exists({ _id: id })
+
+    if (!receiverExists)
+      return res.status(404).json({ message: "Receiver does not exist" })
+
+    let imageUrl
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image)
+      imageUrl = uploadResponse.secure_url
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId: id,
+      text,
+      image: imageUrl,
+    })
+
+    await newMessage.save()
+    res.status(201).json(newMessage)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: "Something went wrong" })
+  }
+}
+
+export const getChatPartners = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id
+
+    const messages = await Message.find({
+      $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
+    })
+
+    const chatPartnerIds = [
+      ...new Set(
+        messages.map((msg) =>
+          msg.senderId.toString() === loggedInUserId.toString()
+            ? msg.receiverId.toString()
+            : msg.senderId.toString()
+        )
+      ),
+    ]
+
+    const chatPartners = await User.find({
+      _id: { $in: chatPartnerIds },
+    }).select("-password")
+
+    res.status(200).json(chatPartners)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: "Something went wrong" })
+  }
+}
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,5 +1,20 @@
 import express from "express"
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessageByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js"
+import { arcjetProtection } from "../middleware/arcjet.middleware.js"
 
 const router = express.Router()
+
+router.use(arcjetProtection)
+
+router.get("/contacts", protectRoute, getAllContacts)
+router.get("/chats", protectRoute, getChatPartners)
+router.get("/:id", protectRoute, getMessageByUserId)
+router.post("/send/:id", protectRoute, sendMessage)
 
 export default router
